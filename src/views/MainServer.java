a5424339fff798d2879ea3e3e79c53d8403b0f1b
Umar <kidzeclipes@yahoo.com>
2016-01-19 00:13:11 +0800

    fixed main

--- src/views/MainServer.java
+++ src/views/MainServer.java
@@ -15,7 +15,13 @@ public class MainServer {
     
     public static void main(String[] args) {
         
-        String ipPublic1 = (args[0] != null) ? (args[0]) : (RMIConn.ipPublic);
+        String ipPublic1 = "";
+        
+        try {
+            ipPublic1 = (args[0] != null) ? (args[0]) : (RMIConn.ipPublic);
+        } catch (Exception e) {
+            ipPublic1 = RMIConn.ipPublic;
+        }
         
         System.out.println("Starting server ...");
         RMIConn.startServer(ipPublic1);
